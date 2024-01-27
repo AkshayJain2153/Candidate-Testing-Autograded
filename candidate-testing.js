12b0b88ddccb79954e75a31df788cd7c3dcdf19e
Akshay <122316281+AkshayJain2153@users.noreply.github.com>
2024-01-27 18:42:12 -0500

    <Part-1>

--- candidate-testing.js
+++ candidate-testing.js
@@ -47,7 +47,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-  console.log(`Hello, ${candidateName} welcome`);
+  console.log(`Hello, ${candidateName} welcome here`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
